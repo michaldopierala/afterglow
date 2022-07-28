ed7f9fae4b88349f5f9bb41299e02c3f2d4a0732
Sam Smith <sam@example.com>
2022-07-28 17:15:35 +0200

    dynamic links

---
+++ urls.php
@@ -0,0 +1,138 @@
+<?php
+$v= array(
+
+
+  // YOU MUST CHANGE THE RIGHT PART OF HTTP://  ADRESS  !!!!!!!
+"main_url" => "http://localhost/sklep/" , 
+
+// YOU MUST CHANGE DATA NAME  !!!!!!!!!!!!!  
+
+ // DATABASE 
+"servername" => "localhost",
+"username" => "root",
+"password" => "",
+"dbname" => "my_db",
+
+
+
+
+
+//MENU
+"logo"=> "http://localhost/sklep/img/logo9-1.png",                 // on server
+"logo_name" => "http://localhost/sklep/img/logo9-1.png",           // on server
+"logo_palm" => "http://localhost/sklep/img/left_logo_black.png",   // on server
+"shipping_icon1" => "http://localhost/sklep/img/cart7.png",        // on server
+"shipping_icon2" => "http://localhost/sklep/img/cart8.png",        // on server   
+"optional_currency" => "http://localhost/sklep/admin/optionalcurrency.php?q=",
+"cart_count" => "http://localhost/sklep/shop/cart_count.php",
+"shopping_cart_link" => "http://localhost/sklep/shop/shopping_cart.php",
+"menu_link1" => "http://localhost/sklep/",    
+"menu_link2" => "http://localhost/sklep/page/portfolio_bracelates.php",
+"menu_link3" => "http://localhost/sklep/product/id2/",
+"menu_link4" => "http://localhost/sklep/page/portfolio_pants.php",
+"menu_link5" => "http://localhost/sklep/page/contact-us.php",
+
+
+// FOOTER
+"foo_join"=> "http://localhost/sklep/img/footer/free3.png", 
+"foo_instagarm"=> "http://localhost/sklep/img/footer/instagram.png",  
+"foo_ins_link"=> "https://www.instagram.com/afterglow_fashion/",   
+"foo_pinterest"=> "http://localhost/sklep/img/footer/pinterest.png",
+"foo_pin_link"=> "",     
+"foo_facebook"=> "http://localhost/sklep//img/footer/fb.png",
+"foo_fac_link"=> "https://www.facebook.com/Afterglow-100552735510329",  
+"foo_pri"=> "",  
+"foo_ter"=> "",  
+"foo_ret"=> "",  
+"foo_shi"=> "",  
+"foo_log"=> "http://localhost/sklep/img/footer/logo9-1.png",  
+"foo_fredom"=> "http://localhost/sklep/img/footer/free2.png", 
+"foo_palm"=> "http://localhost/sklep/img/footer/logo8-1.png", 
+
+
+
+//SUCCES.PHP
+"send_email" => "http://localhost/sklep/shop/payment/email/index.php",
+"email_template"=> "http://localhost/sklep/shop/payment/email/email_template.html",
+
+//CHECKOUT PAGE 
+"payment_image" => "http://localhost/sklep/img/payment_method2.jpg",
+"calculate_shipping" => "http://localhost/sklep/shop/payment/calculate_shipping.php?country=",
+
+//SHOPPING CART
+"checkout_button" => "http://localhost/sklep/shop/payment/checkout.php",
+"product_page" => "http://localhost/sklep/product/id",                      // there was duplicate name 
+"img_remove" => "http://localhost/sklep/img/remove.v2.png",              // on server
+"img_remove_2" => "http://localhost/sklep/img/remove.png",                  // on server
+"back_img" => "http://localhost/sklep/images/cover/bg6.jpg",  
+"back_img_mobile" => "http://localhost/sklep/images/cover/cover_mobile3.jpg",
+"shipping_cost" => "http://localhost/sklep/shop/payment/calculate_shipping.php",
+"cart" => "http://localhost/sklep/shop/cart.php",                          // on server
+
+//indx page
+"url1"=>"35",
+"url2"=>"37",
+"slider1"=>"http://afterglow-fashion.com/images/photo-1494067.jpeg",
+"slider2"=>"http://afterglow-fashion.com/images/frontpage/slider2.2.jpeg",
+"slider3"=>"http://afterglow-fashion.com/images/photo-1494067.jpeg",
+"slider4"=>"http://afterglow-fashion.com/images/frontpage/slider2.2.jpeg",
+"cover_index"=>"http://localhost/sklep/images/cover/cover1.jpg",
+
+
+
+// my comment
+"url3"=>"43",
+
+//IMG
+"payment_method_1"=>"http://localhost/sklep/img/credit_card_payment.jpeg",
+"payment_method_2"=>"http://localhost/sklep/img/paypal.png",
+"sizes_image"=>"http://localhost/sklep/images/size.jpg",
+"top_background_img" => "http://localhost/sklep/images/cover/back_pink.jpg",
+
+
+
+// FILES
+"update_currency"=> "http://afterglow-fashion.com/admin/optionalcurrency_update.php",
+"products"=> "http://localhost/sklep/product",
+
+
+
+// INSTAGARM GRID IMAGES  
+"inst_img1"=>"http://afterglow-fashion.com/images/frontpage/instagram/g4.1x1.jpg",
+"inst_img2"=>"",
+"inst_img3"=>"",
+
+
+// PAGES ADRESSES 
+"stock_link"=>"http://localhost/sklep/admin/stock/index.php",
+"orders_link"=>"http://localhost/sklep/admin/orders/index.php",
+"portfolio_link"=>"http://localhost/sklep/admin/portfolio/index.php",
+
+"addnewproduct_link"=>"http://localhost/sklep/admin/stock/addnewproduct.php",
+"updateaddnewproduct_link"=>"http://localhost/sklep/admin/stock/update.php",
+"admin_link"=>"http://localhost/sklep/admin",
+"showproduct_link"=>"http://localhost/sklep/admin/stock/show_product.php",
+"updatecurrency_link"=>"http://localhost/sklep/admin/optionalcurrency_update.php",
+
+"admin_portfolio_pants"=>"http://localhost/sklep/admin/portfolio/portfolio1.php",
+"admin_portfolio_bracelates"=>"http://localhost/sklep/admin/portfolio/portfolio_bracelates.php",
+"payment_page"=>"http://localhost/sklep/shop/payment/payment.php",
+"success_page"=>"http://localhost/sklep/shop/payment/success.php",
+"product_page_jewelry" => "http://localhost/sklep/product/jewelry/id",                      // there was duplicate name 
+"contact_us_succes" => "http://localhost/sklep/page/contactussuccess.php",  
+
+
+// ORDERS
+"change_status"=>"http://localhost/sklep/admin/orders/change_status.php",
+"print_file"=>"http://localhost/sklep/admin/orders/print.php?value_key=",
+"or_neworders"=>"http://localhost/sklep/admin/orders/new.php",
+"or_allorders"=>"http://localhost/sklep/admin/orders/index.php",
+"or_completed"=>"http://localhost/sklep/admin/orders/completed_orders.php",
+"or_statistics"=>"http://localhost/sklep/admin/orders/statistics.php",
+""=>"",
+
+
+
+);
+
+?>
